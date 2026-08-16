ac9d153859bbe18b73ccca29f870942589963ed5
Nik-s-har <nick762@yandex.ru>
2026-08-16 11:30:15 +0300

    Add GitHub Pages deploy from develop

--- src/router.tsx
+++ src/router.tsx
@@ -5,16 +5,19 @@ import { PrivacyPage } from './pages/PrivacyPage/PrivacyPage';
 import { alphabetRoutes } from '@alphabet/routes.tsx';
 import { trainerRoutes } from '@trainers/routes.tsx';
 
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      { path: '/', element: <MainPage /> },
-      { path: '/privacy', element: <PrivacyPage /> },
-      ...alphabetRoutes,
-      ...trainerRoutes,
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      element: <Layout />,
+      children: [
+        { path: '/', element: <MainPage /> },
+        { path: '/privacy', element: <PrivacyPage /> },
+        ...alphabetRoutes,
+        ...trainerRoutes,
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL },
+);
 
 export default router;
